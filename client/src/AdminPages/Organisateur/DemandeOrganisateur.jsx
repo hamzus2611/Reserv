63358c42b78053c7c8eb3cb61358409cab6976fa
hamzus2611 <hamza.gharsallah@isimg.tn>
2022-05-23 17:33:41 +0100

    first commit

---
+++ client/src/AdminPages/Organisateur/DemandeOrganisateur.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DataOrgaDemand from "../../AdminComponents/chart/datatable/DataOrgaDemand";
+
+function DemandeOrganisateur() {
+  return (
+    <div>
+      <DataOrgaDemand />
+    </div>
+  );
+}
+
+export default DemandeOrganisateur;
